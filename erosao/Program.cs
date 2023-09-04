bf36dc6593431da115c94bd2724a2bf0472861aa
Enzo França <Enzofran4@gmail.com>
2023-09-04 14:03:13 -0400

    first commit

---
+++ erosao/Program.cs
@@ -0,0 +1,15 @@
+﻿class erosao
+{
+    static void Main(string[] args)
+    {
+        List <string> ListaString = new List <string> ();   
+        ListaString.Add ("a");
+        ListaString.Add ("b");  
+        ListaString.Add ("c");
+
+         foreach (string str in ListaString)
+        {
+            Console.WriteLine (str);    
+        }
+    }
+}
